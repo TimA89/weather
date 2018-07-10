076c4487e7943f2cf9ddade7debebf95d17c0206
TimA89 <timabuladze@gmail.com>
2018-07-09 21:24:30 -0400

    Added WeatherDisplay component and Making API calls
    - Added WeatherDisplay component that uses componentDidMount method to
    make a call and store JSON date in .state for WeatherDisplay component
    
    - structuring a data on return and specifying how we will see it
    
    - PLACES var holds a city names and zip codes for us to pass into URL
    and use name to display it on the button
    
    - App component holds buttons, providing them with city names and
    triggers displaying of information on event 'onClick'

--- src/App.js
+++ src/App.js
@@ -1,40 +1,77 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 const PLACES = [
-  { name: "Palo Alto", zip: "94303" },
-  { name: "San Jose", zip: "94088" },
-  { name: "Santa Cruz", zip: "95062" },
-  { name: "Honolulu", zip: "96803" }
-];
+  { name: "Boston", zip: "02108" },
+  { name: "Los Angeles", zip: "90014" },
+  { name: "New York", zip: "10023" },
+  { name: "Honolulu", zip: "96813" }
+]
 
 class WeatherDisplay extends Component {
+  constructor() {
+    super()
+    this.state = {
+      weatherData: null
+    }
+  }
+
+  componentDidMount() {
+    const zip = this.props.zip
+    const URL = "http://api.openweathermap.org/data/2.5/weather?zip=" +
+      zip + ",us" +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial"
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json })
+    })
+  }
   render() {
+    const weatherData = this.state.weatherData
+    if (!weatherData) return <div>Loading</div>
+    const weather = weatherData.weather[0]
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png"
     return (
-      <h1>Displaying weather for city {this.props.zip}</h1>
-    );
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    )
   }
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      activePlace: 0
+    }
+  }
+
   render() {
+    const activePlace = this.state.activePlace
     return (
       <div className="App">
-      <WeatherDisplay zip={"12345"} />
+      <WeatherDisplay zip={PLACES[activePlace].zip}
+      key={activePlace} />
       {PLACES.map((place, index) => (
       <button
         key={index}
         onClick={() => {
-          console.log('Clicked index '+index);
+          this.setState({ activePlace: index })
         }}
       >
           {place.name}
       </button>
     ))}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
